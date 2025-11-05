93e87c19b0bb8749cd0b9c1a54eadc147b7ba841
Mr. Burton <benjaminlburton@gmail.com>
2025-11-05 00:39:22 -0500

    making small changes

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "CLI Native",
+    pageTitleSuffix: "Master the Command Line. Automate Everything.",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
